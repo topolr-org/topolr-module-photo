5ba1a7ddcbdbe339962e81773e2cc2cec3c216f2
Jinliang <hou80houzhu@vip.qq.com>
2017-05-05 14:51:14 +0800

    init

--- com/topolr/photo/service/showservice.js
+++ com/topolr/photo/service/showservice.js
@@ -7,21 +7,31 @@ Module({
     extend:"localservice",
     action_set:function(option){
         this.data=$.extend(true,{},option);
-        this.data.bigImages=[];
         this.data.thumbImages=[];
         for(var i=0;i<this.data.images.length;i++){
             var image=this.data.images[i];
-            if(image.big) {
-                this.data.bigImages.push(image.big);
-            }
             if(image.thumb){
-                this.data.thumbImages.push(image.thumb);
+                this.data.thumbImages.push({
+                    img:image.thumb,
+                    index:i
+                });
             }
         }
         this.trigger();
         this.start();
     },
     service_setimages:function (array) {
-
+        this.data.images=this.data.images.concat(array);
+        this.data.thumbImages=[];
+        for(var i=0;i<this.data.images.length;i++){
+            var image=this.data.images[i];
+            if(image.thumb){
+                this.data.thumbImages.push({
+                    img:image.thumb,
+                    index:i
+                });
+            }
+        }
+        this.trigger();
     }
 });
