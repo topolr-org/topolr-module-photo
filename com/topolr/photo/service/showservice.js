0de2eab33ef14d8366b85db243d59c6885f45e08
Jinliang <hou80houzhu@vip.qq.com>
2017-05-05 19:42:32 +0800

    init

--- com/topolr/photo/service/showservice.js
+++ com/topolr/photo/service/showservice.js
@@ -5,18 +5,28 @@
 Module({
     name:"galleryservice",
     extend:"localservice",
+    goto:function (index) {
+        if(index>=0&&index<this.data.images.length){
+            this.data.current=index;
+            this.trigger();
+        }
+    },
     action_set:function(option){
         this.data=$.extend(true,{},option);
         this.data.thumbImages=[];
+        this.data.current=0;
         for(var i=0;i<this.data.images.length;i++){
             var image=this.data.images[i];
             if(image.thumb){
                 this.data.thumbImages.push({
                     img:image.thumb,
-                    index:i
+                    index:i,
+                    active:false
                 });
             }
         }
+        this.data.thumbImages[this.data.current].active=true;
+        this.data.currentImage=this.data.images[this.data.current];
         this.trigger();
         this.start();
     },
